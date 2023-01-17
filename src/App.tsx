fa65b975acb8231206a0d3b84cbc309cc826d1b5
FZsiga02 <fzsigmond02@gmail.com>
2023-01-17 08:45:47 +0100

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,55 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='calculator'>
+        <label>Írja be cm-ben számított testmagasságát (cipő nélkül mérve): </label>
+        <input type="number" id='height'/>
+        <label> cm</label><br/>
+
+        <label>Írja be kg-ban számított testsúlyát (cipő nélkül mérve): </label>
+        <input type="number" id='mass'/>
+        <label> kg</label><br/>
+
+        <button id='calculate'>Számítás</button>
+      </div>
+      <div id='tableDiv'>
+        <p>Az ön BMI értéke: </p><output id='BMI'></output><br/>
+        <table id='table'>
+          <tr>
+            <td>18,5 vagy kevesebb</td>
+            <td>Sovány</td>
+          </tr>
+          <tr>
+            <td>18,5-24,9</td>
+            <td>Normál</td>
+          </tr>
+          <tr>
+            <td>25-29,9</td>
+            <td>Túlsúly</td>
+          </tr>
+          <tr>
+            <td>30-34,9</td>
+            <td>I. fokú elhízás</td>
+          </tr>
+          <tr>
+            <td>35-39,9</td>
+            <td>II. fokú elhízás</td>
+          </tr>
+          <tr>
+            <td>40 vagy több</td>
+            <td>Normál</td>
+          </tr>
+        </table>
+      </div>
+      <div id='idealDiv'>
+        <p>Az ideális testtömge: </p><output id='ideal'></output><br/>
+        <table id='idealTable'>
+          <tr>
+            <td><output id='idealValues'></output></td>
+            <td>Kívánatos szélső érték</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
